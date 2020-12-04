0511466550ae4f37ea8c9c66c07469df22233339
Ivan Krasin'kov <tirelspirit@mail.ru>
2020-12-04 13:55:56 +0300

    start

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+	  <component :is="layout">
+		  <router-view />
+	  </component>
+	  <button class="btn">test</button>
+    <router-view />
+  </div>
+</template>
+
+<script>
+import EmptyLayout from '../src/layouts/EmptyLayout'
+import MainLayout from '../src/layouts/MainLayout'
+export default {
+	computed: {
+		layout(){
+			console.log()
+			return (this.$route.meta.layout || 'empty') + '-layout'
+		}
+	},
+	components: {
+		EmptyLayout, MainLayout
+	}
+}
+</script>
+
+<style>
+@import '../node_modules/materialize-css/dist/css/materialize.min.css';
+@import 'assets/index.css';
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="home">
+    Lorem ipsum dolor sit amet.
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home"
+};
+</script>
